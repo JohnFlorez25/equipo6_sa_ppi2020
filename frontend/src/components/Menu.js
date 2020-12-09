0c973676d4e05bfa38050dfa8e03d0ef4c903e06
InfernalRexPain <cygnynml@gmail.com>
2020-12-09 10:59:19 -0500

    Project

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -1,25 +1,37 @@
 import React from "react";
-import {Link, withRouter} from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 const Menu = (props) => {
-const {history}=props;
+  const { history } = props;
   return (
     <div className="Menu">
-    
-    <center><h1>Principal</h1></center>
-    <button type="button" class="btn btn-primary btn-lg btn-block">
-      Procesos geológicos planetarios
-    </button>
-    <p Header="margin: 10px" />
-    <button type="button" class="btn btn-secondary btn-lg btn-block">
-      Information
-    </button>
-    <p Header="margin: 10px" />
-    <button type="button" class="btn btn-secondary btn-lg btn-block">
-      Solutions
-    </button>
-  </div>
-);
+      <center>
+        <h1>Principal</h1>
+      </center>
+      <button
+        type="button"
+        class="btn btn-primary btn-lg btn-block"
+        onClick={() => history.push("/P1Level")}
+      >
+        Procesos geológicos planetarios
+      </button>
+      <p Header="margin: 10px" />
+      <button
+        type="button"
+        class="btn btn-secondary btn-lg btn-block"
+        onClick={() => history.push("/P1Answer1")}
+      >
+        Information
+      </button>
+      <p Header="margin: 10px" />
+      <button
+        type="button"
+        class="btn btn-secondary btn-lg btn-block"
+        onClick={() => history.push("/Solutions")}
+      >
+        Solutions
+      </button>
+    </div>
+  );
 };
 
 export default Menu;
-      
--- frontend/src/components/P1Level.js
+++ frontend/src/components/P1Level.js
@@ -1,32 +1,62 @@
 import React from "react";
-import {Link, withRouter} from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 const P1Level = (props) => {
-const {history}=props;
+  const { history } = props;
   return (
     <div className="Op2Nivel2">
       <header>
-      <nav className="navbar navbar-light bg-light">
-   <span className="navbar-brand mb-0 h1">NIVEL 2</span>
-  </nav>  
-    </header>
-      <h3 className="Titulo">
-      Consecuencias
-      </h3>
+        <nav className="navbar navbar-light bg-light">
+          <span className="navbar-brand mb-0 h1">NIVEL 2</span>
+        </nav>
+      </header>
+      <h3 className="Titulo">Consecuencias</h3>
       <div className="Consecuencias">
-      <strong><p>Las consecuencias más evidentes de las catástrofes de origen natural o geológico son:</p></strong>
-          <p>En el caso de los terremotos: Movimiento y ruptura del suelo, corrimientos y deslizamientos de tierra, incendios, licuefacción del suelo, inundaciones e impactos humanos</p>
-          <p>Los impactos humanos de los terremotos son: Un sismo puede causar lesiones o incluso pérdidas de vidas, daños en las carreteras y puentes, daño general de los bienes, y colapso o desestabilización de edificios. También puede ser el origen de enfermedades, falta de necesidades básicas, y primas de seguros más elevadas. </p>
-          <p>En el caso de los maremotos: Debido a que la energía de los tsunamis tectónicos es casi constante, pueden llegar a cruzar océanos y afectar a costas muy alejadas del lugar del suceso. La trayectoria de las ondas puede modificarse por las variaciones del relieve abisal, fenómeno que no ocurre con las olas superficiales. A medida que un tsunami se aproxima a la costa, puede ascender varios metros o, en raras ocasiones, decenas de metros, y causar la pérdida de muchas vidas y daños a la propiedad al tocar tierra, inundación, erosión, incendios, contaminación del agua potable, brotes de infecciones y enfermedades, y colapso de construcciones</p>
-          <p>Hay más acontecimientos geológicos, pero ésta sería una breve descripción</p>
-          
-          <img className="Img1" src="./img/Img1.jpeg" alt="Img" />
+        <strong>
+          <p>
+            Las consecuencias más evidentes de las catástrofes de origen natural
+            o geológico son:
+          </p>
+        </strong>
+        <p>
+          En el caso de los terremotos: Movimiento y ruptura del suelo,
+          corrimientos y deslizamientos de tierra, incendios, licuefacción del
+          suelo, inundaciones e impactos humanos
+        </p>
+        <p>
+          Los impactos humanos de los terremotos son: Un sismo puede causar
+          lesiones o incluso pérdidas de vidas, daños en las carreteras y
+          puentes, daño general de los bienes, y colapso o desestabilización de
+          edificios. También puede ser el origen de enfermedades, falta de
+          necesidades básicas, y primas de seguros más elevadas.{" "}
+        </p>
+        <p>
+          En el caso de los maremotos: Debido a que la energía de los tsunamis
+          tectónicos es casi constante, pueden llegar a cruzar océanos y afectar
+          a costas muy alejadas del lugar del suceso. La trayectoria de las
+          ondas puede modificarse por las variaciones del relieve abisal,
+          fenómeno que no ocurre con las olas superficiales. A medida que un
+          tsunami se aproxima a la costa, puede ascender varios metros o, en
+          raras ocasiones, decenas de metros, y causar la pérdida de muchas
+          vidas y daños a la propiedad al tocar tierra, inundación, erosión,
+          incendios, contaminación del agua potable, brotes de infecciones y
+          enfermedades, y colapso de construcciones
+        </p>
+        <p>
+          Hay más acontecimientos geológicos, pero ésta sería una breve
+          descripción
+        </p>
 
-      <center><button 
-      type="submit" 
-      class="btn btn-success btn-lg"
-      onClick={() => history.push("/CuestionarioN2")}>
-      Continuar
-      </button></center>
+        <img className="Img1" src="./img/Img1.jpeg" alt="Img" />
+
+        <center>
+          <button
+            type="submit"
+            class="btn btn-success btn-lg"
+            onClick={() => history.push("/P1LevelTurb")}
+          >
+            Continuar
+          </button>
+        </center>
       </div>
     </div>
   );
